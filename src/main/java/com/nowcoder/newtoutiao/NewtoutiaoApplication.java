73c9a08785a7eb6f658fb32d57b7b3b6a45ed1c7
wulening <wulening@foxmail.com>
2018-11-05 22:00:34 +0800

    牛客网中级课头条项目

---
+++ src/main/java/com/nowcoder/newtoutiao/NewtoutiaoApplication.java
@@ -0,0 +1,12 @@
+package com.nowcoder.newtoutiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtoutiaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtoutiaoApplication.class, args);
+	}
+}
